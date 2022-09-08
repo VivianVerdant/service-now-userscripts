80154420e1cdc3f05dfe3006843f0215c620c76c
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2022-09-08 14:30:16 -0500

    Update Better KB Links.user.js

--- Better KB Links.user.js
+++ Better KB Links.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         Better KB Links
 // @namespace    https://github.com/VivianVerdant/
-// @version      0.5
+// @version      0.6
 // @description  Always open KBs in new tabs and choose between viewing minimal and full versions
 // @author       Vivian
 // @match        https://*.service-now.com/*
-// @grant        none
+// @require      https://github.com/VivianVerdant/service-now-userscripts/raw/main/waitForKeyElements.js
+// @grant        GM_addStyle
 // ==/UserScript==
 
 /* Changelog
+v0.6 - Made the script much more reliable and work globally.
 v0.5 - Added static button that lists KB number and copies it to clipboard when clicked.
 v0.4 - REALLY fixed duplicate link creation this time
 v0.3 - Creates correct links inside of KBs and for permalinks
@@ -16,13 +18,21 @@ v0.2 - Fixes links generated after page load and doesn't sometimes create duplic
 v0.1 - Initial release
 */
 
+/* globals waitForKeyElements */
+
 var KB;
 
+var run_once = false;
+
 function replaceKBlinks(){
+	//waitForKeyElements("a[href*='id=kb_article_view']", main, true);
+	console.warn("replace links");
 	var qLinks = document.querySelectorAll("a[href*='id=kb_article_view']");
+	console.warn(qLinks.length);
 
 	for (var J = qLinks.length - 1; J >= 0; --J) {
-		if (qLinks[J].getAttribute ('class')=="handled"){
+		console.log(qLinks[J]);
+		if (qLinks[J].hasAttribute('handled')){
 			continue
 		}
 		var oldHref = qLinks[J].getAttribute ('href');
@@ -33,26 +43,21 @@ function replaceKBlinks(){
 			newHref = oldHref.replace (/(\?id=kb_article_view&)/, "kb_view.do?");
 			oldHref = "https://virteva.service-now.com/kb" + oldHref;
 		}
-		//console.log (oldHref + "\n" + newHref);
+		console.log (oldHref + "\n" + newHref);
 		qLinks[J].setAttribute('href', newHref);
 		qLinks[J].setAttribute("target", "_blank");
-		qLinks[J].setAttribute('class', "handled");
+		qLinks[J].setAttribute('handled', true);
 
 		var buttonNew = document.createElement("a");
 		buttonNew.setAttribute("href", oldHref);
 		buttonNew.setAttribute("target", "_blank");
 		buttonNew.setAttribute("title", oldHref);
-		buttonNew.setAttribute("class", "handled");
+		buttonNew.setAttribute("handled", true);
 		buttonNew.innerHTML = "\(Full View\)   ";
 		qLinks[J].parentElement.insertBefore(buttonNew, qLinks[J]);
 	}
 }
 
-function waitforme(milisec) {
-    return new Promise(resolve => {
-        setTimeout(() => { resolve('') }, milisec);
-    })
-}
 
 var btn_style = "position: fixed;"+
   "width: max-content;"+
@@ -72,15 +77,20 @@ function kbToClipboard(){
 	  navigator.clipboard.writeText(KB);
 }
 
-async function buttonifyKB(){
+function waitforme(milisec) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve('') }, milisec);
+    })
+}
+
+async function kb_view_page(){
+	//console.warn("view page");
 	let el = null;
 	if (document.location.href.includes("kb_view.do")){
-		console.warn("foo");
 		setTimeout(() => { el = "none"; console.warn("Can't find el D:") }, 5000);
 		while (el == null){
 			try{
 				el = document.getElementById("articleNumberReadonly");
-				console.warn("foo ", el.innerHTML);
 			}catch(e){
 				console.warn(e);
 			}
@@ -90,26 +100,23 @@ async function buttonifyKB(){
 		KB = el.innerHTML.match(reg);
 
 	}else{
-		console.warn("bar");
 		let spans = document.getElementsByClassName("ng-binding");
-		setTimeout(() => { el = "none"; console.warn("Can't find el D:") }, 5000);
+		setTimeout(() => {el = "none";}, 5000);
 		while (el == null){
 			await waitforme(250);
 			//spans = document.getElementsByClassName("ng-binding");
-			console.warn(spans.length);
 			if (spans.length < 2) {continue};
 			for (let i in spans){
 				if (spans[i].innerHTML.includes("KB") && spans[i].innerHTML.length < 20){
 					//if (spans[i].hasChildNodes){continue};
 					el = spans[i];
-					console.warn("bar ", el.innerHTML);
 					break
 				}
 			}
 		}
 		KB = el.innerHTML;
 	}
-	console.warn(KB);
+	//console.warn(KB);
 	let btn = document.createElement("button");
 	btn.setAttribute("style", btn_style);
 	document.body.appendChild(btn)
@@ -117,25 +124,37 @@ async function buttonifyKB(){
 	let inner = document.createElement("span");
 	inner.innerHTML = KB;
 	btn.appendChild(inner);
-	console.log(btn);
+	//console.log(btn);
+}
+
+async function kb_search_page(){
+	//console.warn("search page");
+	if (run_once){
+		return;
+	}
+	run_once = true
+
+	var origOpen = XMLHttpRequest.prototype.open;
+	XMLHttpRequest.prototype.open = function(method, url) {
+		this.addEventListener('load', function() {
+			//console.log('Degugging', method, url);
+			replaceKBlinks();
+		});
+		origOpen.apply(this, arguments);
+	};
 }
 
 function main() {
+	//console.warn("start main");
     'use strict';
-	if (window.location.href.includes("sysparm_article") || window.location.href.includes("kb_article_view") || window.location.href.includes("kb_view.do")){
-		replaceKBlinks();
-		buttonifyKB();
-		return;
-	}
-
-
-	var origOpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url) {
-        this.addEventListener('load', function() {
-            //console.log('Degugging', method, url);
-            replaceKBlinks();
-        });
-        origOpen.apply(this, arguments);
-    };
+	replaceKBlinks();
 }
-main();
+console.warn("Starting better KB links");
+var loc = window.location.href;
+if (loc.includes("kb_search")){
+		kb_search_page();
+}else if(loc.includes("kb_article_view")){
+	kb_view_page();
+}
+waitForKeyElements("a[href*='id=kb_article_view']", main, true);
+console.warn("Ending better KB links");

0a93bf6b7f6169e1ccbcda492a4b5d3515917a9d
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2022-09-17 16:50:20 -0500

    Update better_settings_menu.js

--- better_settings_menu.js
+++ better_settings_menu.js
@@ -1,22 +1,20 @@
 // Get all the dropdown from document
-function createBetterSettingsMenu(){
+function createBetterSettingsMenu(e){
     var cssTxt = GM_getResourceText("customCSS");
 	GM_addStyle (cssTxt);
 
 	var menu_html = '<div class="better-menu better-dropdown-container better-click-dropdown"></div>'+
-                      '<ul class="better-menu better-dropdown-menu">'+
-                        '<li>Better Service-Now Settings</li>'+
-                         '<li>Keep Company Filter<input class="better-menu tgl-hidden tgl-skewed" id="cb3" type="checkbox" />'+
-                        '<label class="better-menu better-tgl-btn" data-tg-off="OFF" data-tg-on="ON" for="cb3"></label></li>'+
-                      '</ul>';
+		      '<ul class="better-menu better-dropdown-menu">'+
+			'<li>Better Service-Now Settings</li>'+
+			 '<li>Keep Company Filter<input class="better-menu tgl-hidden tgl-skewed" id="cb3" type="checkbox" />'+
+			'<label class="better-menu better-tgl-btn" data-tg-off="OFF" data-tg-on="ON" for="cb3"></label></li>'+
+		      '</ul>';
 
-	var header = document.querySelectorAll("div.navbar-right")[0].firstChild;
 	better_options_btn = document.createElement("div");
 	better_options_btn.setAttribute("style", "float: right;");
 	better_options_btn.innerHTML = menu_html;
-	
-	header.insertAdjacentElement('afterbegin', better_options_btn);
-    
+
+	e.insertAdjacentElement('afterbegin', better_options_btn);
     
     document.querySelectorAll(".better-dropdown-container").forEach(dropDownFunc);
 }

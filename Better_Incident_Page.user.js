20c4d0669efd569a71d59503bf8926f6ae6e97e2
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2022-11-05 13:02:15 -0500

    Update Better_Incident_Page.user.js

--- Better_Incident_Page.user.js
+++ Better_Incident_Page.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Incident Page
 // @namespace    https://github.com/VivianVerdant/
-// @version      0.5
+// @version      0.7
 // @description  Description
 // @author       Vivian
 // @match        https://*.service-now.com/*

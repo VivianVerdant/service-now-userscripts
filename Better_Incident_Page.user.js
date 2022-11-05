913046fc0c3793e942aa83557a78df73f5cbfbc8
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2022-11-05 13:01:01 -0500

    Update Better_Incident_Page.user.js

--- Better_Incident_Page.user.js
+++ Better_Incident_Page.user.js
@@ -14,6 +14,7 @@
 // ==/UserScript==
 
 /* Changelog
+v0.7 - refactor
 v0.6 - overhaul
 v0.5 - Made Incident fields that are mandatory but not filled out have a more prominent styling.
 v0.4 - Made company notes on Incident edit page into a toggleable static div.
@@ -27,7 +28,6 @@ v0.1 - Initial release
 
 'use strict';
 
-var INC;
 var location = window.location.href;
 var run_once = false;
 var better_options_btn;
@@ -45,32 +45,73 @@ async function onClickResolveBtn(){
 	console.log(close_field);
 }
 
-function kbToClipboard(){
+function kbToClipboard(e){
+	const INC = document.getElementById('incident.number').value;
 	navigator.clipboard.writeText(INC);
-	this.classList.add("icon-cog");
-	setTimeout(() => {this.classList.remove("icon-cog");}, 500);
+	e.target.classList.add("icon-cog");
+	setTimeout(() => {e.target.classList.remove("icon-cog");}, 500);
 
 }
 
-function find_or_await_element(query, func) {
-	const node = document.querySelectorAll(query)[0];
-	if (node){func(node);return}
-	const observer = new MutationObserver(function(mutations_list) {
-		mutations_list.forEach(function(mutation) {
-			if (mutation.type != "childList") {
+function find_or_observe_for_element(query, func, parent_query, once) {
+
+	let parent_node;
+
+	if (parent_query === undefined) {
+		parent_node = document.body;
+	} else {
+		parent_node = document.querySelector(parent_query);
+	}
+
+	if (once === undefined) {
+		once = true;
+	}
+
+	const node_list = parent_node.querySelectorAll(query);
+	//console.log("node list: ", node_list);
+	if (node_list.length) {
+		for (const node of node_list) {
+			//console.log("found node already existing: ", node);
+			func(node);
+			if (once) {
+				//console.log("exiting early");
 				return;
 			}
-			for (const added_node of mutation.addedNodes) {
-				if (added_node.nodeType != 1) {
+		}
+	}
+
+	const observer = new MutationObserver((mutations_list, observer) => {
+		//console.log("callback from: ", observer.original_query);
+		//console.log(mutations_list);
+		const mutation_fn = (n_list) => {
+			//console.log(n_list);
+			/*if (m_list.type !== "childList") {
+				return;
+			}*/
+			for (const added_node of n_list) {
+				//console.log("added node: ", added_node);
+				if (added_node.nodeType !== Node.ELEMENT_NODE) {
 					continue;
 				}
 				if (added_node.matches(query)) {
 					func(added_node);
+					if (once) {
+						observer.disconnect();
+						return;
+					}
 				}
 			}
-		});
+		}
+		for (const list of mutations_list) {
+			if (list.type == "childList") {
+				//console.log("child list: ", list);
+				mutation_fn(list.addedNodes);
+			}
+		}
 	});
-	observer.observe(document.documentElement, { subtree: true, childList: true });
+	Object.defineProperty(observer, "original_query", {value: query, writable: true});
+	observer.observe(parent_node, { subtree: true, childList: true });
+	//console.log("observer: ", observer);
 }
 
 function main(element) {
@@ -82,27 +123,26 @@ function main(element) {
 
 	GM_addStyle(GM_getResourceText("better_incident_css"));
 
-	find_or_await_element(".outputmsg", function(node) {
+	find_or_observe_for_element(".outputmsg", (node) => {
 		console.log('.outputmsg has been added:-------------------------------------------');
-		console.log(node.nodeValue);
-		node.firstElementChild.addEventListener("click", function (e) {
-			if (this.classList.contains("icon-info")) {
-				this.nextSibling.classList.toggle("hidden");
+		console.log(node);
+		node.firstElementChild.addEventListener("click", (e) => {
+			if (e.target.classList.contains("icon-info")) {
+				e.target.nextSibling.classList.toggle("hidden");
 			}
 		});
 		node.lastElementChild.classList.add("hidden");
-	});
-	find_or_await_element("#resolve_incident", function(node) {
+	}, "#output_messages", false);
+	find_or_observe_for_element("#resolve_incident", (node) => {
 		console.log('#resolve_incident has been added:-------------------------------------------');
-		console.log(node.nodeValue);
+		console.log(node);
 		node.addEventListener("click", onClickResolveBtn);
 	});
-	find_or_await_element("input[id='incident.number']", function(node) {
+	find_or_observe_for_element("input[id='incident.number']", (node) => {
 		console.log('#resolve_incident has been added:-------------------------------------------');
-		console.log(node.nodeValue);
 		let btn = document.createElement("button");
 		btn.classList.add("inc_copy_button","btn");
-		let header = document.querySelectorAll("nav.navbar.navbar-default.section_zero > div.container-fluid")[0];
+		let header = document.querySelector("nav.navbar.navbar-default.section_zero > div.container-fluid");
 		header.appendChild(btn);
 		header.insertBefore(btn, btn.previousSibling);
 		btn.addEventListener("click", kbToClipboard);
@@ -110,29 +150,15 @@ function main(element) {
 		inner.innerHTML = node.value;
 		btn.appendChild(inner);
 	});
-	find_or_await_element(".question_textarea_input", function(node) {
-		console.log('.question_textarea_input has been added:-------------------------------------------');
-		console.log(node.nodeValue);
-		node.setAttribute("onchange", 'this.style.height = "";this.style.height = this.scrollHeight + 16 + "px";');
-		node.setAttribute("onkeydown", 'this.style.height = "";this.style.height = this.scrollHeight + 16 + "px";');
-		node.setAttribute("style", "overflow-y: max-content; max-height: 2000px !important; resize: none;");
-	});
-	find_or_await_element("#activity-stream-work_notes-textarea", function(node) {
-		console.log('#activity-stream-work_notes-textarea has been added:-------------------------------------------');
-		console.log(node.nodeValue);
-		node.setAttribute("onchange", 'this.style.height = "";this.style.height = this.scrollHeight + 16 + "px";');
-		node.setAttribute("onkeydown", 'this.style.height = "";this.style.height = this.scrollHeight + 16 + "px";');
-		node.setAttribute("style", "overflow-y: max-content; max-height: 2000px !important; resize: none;");
-		node.style.height = node.scrollHeight + 16 + "px";
-	});
-	find_or_await_element(".activity-stream-textarea", function(node) {
-		console.log('.activity-stream-textarea has been added:-------------------------------------------');
-		console.log(node.nodeValue);
-		node.setAttribute("onchange", 'this.style.height = "";this.style.height = this.scrollHeight + 16 + "px";');
-		node.setAttribute("onkeydown", 'this.style.height = "";this.style.height = this.scrollHeight + 16 + "px";');
-		node.setAttribute("style", "overflow-y: max-content; max-height: 2000px !important; resize: none;");
-		node.style.height = node.scrollHeight + 16 + "px";
-	});
+	find_or_observe_for_element(".activity-stream-textarea, #activity-stream-work_notes-textarea, .question_textarea_input", (node) => {
+		console.log('textarea has been added:-------------------------------------------');
+		console.log(node);
+		const text_area_fn = (e) => {e.target.style.height = "0px"; e.target.style.height = e.target.scrollHeight + 8 + "px";}
+		node.addEventListener("change", text_area_fn);
+		node.addEventListener("keydown", text_area_fn);
+		node.addEventListener("click", text_area_fn);
+		setTimeout(() => {node.click();}, 250);
+	}, undefined, false);
 }
 
 if (location.includes("b47514e26f122500a2fbff2f5d3ee4d0") || location.includes("incident.do")){

d6a5a66e63cd6214f359818d9fed2f0dc1ab306f
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2022-12-28 16:15:48 -0600

    Update find_or_observe_for_element.js

--- find_or_observe_for_element.js
+++ find_or_observe_for_element.js
@@ -1,124 +1,74 @@
 async function find_or_observe_for_element(query, func, parent_query, once) {
+
+	setTimeout(() => {
+		if (document.body === undefined) {
+			console.error("No body found");
+			return;
+		}
+	}, 6000);
+
+	while (document.body === undefined) {
+		setTimeout(() => {;}, 150);
+	}
+
 	let parent_node;
 
-	//console.log(query);
-
-	const compare_fn = (added_node) => {
-		if (added_node.matches(query)) {
-			return true;
-		}
-		return false
+	if (parent_query === undefined) {
+		parent_node = document.body;
+	} else {
+        	setTimeout(() => {
+			if (document.body === undefined) {
+				console.warn("No custom parent found, falling back to body");
+				parent_node = document.body;
+			}
+		}, 10000);
+		while (parent_node === undefined) {
+		    parent_node = document.querySelector(parent_query);
+		    setTimeout(() => {;}, 150);
+        	}
 	}
-
-	function recursive_search(node){
-		let results = [];
-		if (node.nodeType == Node.ELEMENT_NODE && compare_fn(node)) {
-			results.push(node);
+	const node_list = parent_node.querySelectorAll(query);
+	let match_list = [];
+	if (once && node_list.length) {
+		func(node_list[0]);
+	}else if (node_list.length) {	
+		for (const node of node_list) {
+			func(node);
 		}
-		if (node.children){
-			for (const child of node.children) {
-				if (child.nodeType !== Node.ELEMENT_NODE) {
+	}
+	const observer = new MutationObserver((mutations_list, observer) => {
+		let match_list = [];
+		const compare_fn = (added_node) => {
+			if (added_node.matches(query)) {
+				return true;
+			}
+			return false
+		}
+		const mutation_fn = (n_list) => {
+			for (const added_node of n_list) {
+				if (added_node.children){
+					mutation_fn(added_node.children);
+				}
+				if (added_node.nodeType !== Node.ELEMENT_NODE) {
 					continue;
 				}
-				results.push(recursive_search(child));
-			}
-		}
-		console.log("restults: ", results);
-		if (results.length) {
-			return results;
-		} else {
-			return null;
-		}
-
-	}
-
-	async function find_body() {
-		const body_observer = new MutationObserver(() => {
-			let node = document.body;
-			if (node) {
-				parent_node = node;
-				process();
-				body_observer.disconnect();
-			}
-		});
-		body_observer.observe(document.documentElement, {childList: true});
-	}
-
-	async function find_query() {
-		const query_observer = new MutationObserver(() => {
-			let node = document.querySelector(parent_query);
-			if (node) {
-				parent_node = node;
-				process();
-				query_observer.disconnect();
-			}
-		});
-		query_observer.observe(document.documentElement, {childList: true});
-
-		setTimeout(() => {
-			if (document.body === undefined) {
-				console.warn("Can't find query, falling back to body");
-				query_observer.disconnect();
-				find_body();
-			}
-		}, 3000);
-	}
-
-	function process() {
-		const child_observer = new MutationObserver((mutations_list, observer) => {
-			let match_list = [];
-			for(const node of document.querySelectorAll(query).values()) {
-				match_list.push(node);
-			}
-			//console.log("mutations_list: ", mutations_list);
-
-			const recursive_mutations = (obj) => {
-				if (obj.constructor.name == "Array"){
-					obj.forEach( (element) => {
-						recursive_mutations(element);
-					});
-				} else if (obj.constructor.name == "MutationRecord") {
-					obj.addedNodes.forEach( (element) => {
-						recursive_mutations(element);
-					});
-				} else if (obj.nodeType == Node.ELEMENT_NODE) {
-					if (obj.matches(query)) {
-						//console.log("found " + query + ": ", obj);
-						match_list.push(obj);
-					}
-					if (obj.childNodes) {
-						obj.childNodes.forEach( (element) => {
-							recursive_mutations(element);
-						});
-					}
-				} else {
+				if (compare_fn(added_node)) {
+					match_list.push(added_node);
 				}
 			}
-			if (match_list.length && once) {
-
-			} else {
-				recursive_mutations(mutations_list);
+		}
+		for (const list of mutations_list) {
+			if (list.type == "childList") {
+				mutation_fn(list.addedNodes);
 			}
-			//console.log(match_list)
-
-			if (once && match_list.length) {
-				//console.log(match_list[0]);
-				func(match_list[0]);
-				child_observer.disconnect()
-			} else if (match_list.length) {
-				//console.log(match_list);
-				for (const node of match_list) {
-					func(node);
-				}
+		}
+		if (once && match_list.length) {
+			func(match_list[0]);
+		} else if (match_list.length) {
+			for (const node of match_list) {
+				func(node);
 			}
-		});
-		//console.log(child_observer);
-		child_observer.observe(parent_node, { subtree: true, childList: true });
-	}
-
-	if (parent_query === undefined) {
-		find_body();
-	} else {
-		find_query();
-	}
+		}
+	});
+	observer.observe(parent_node, { subtree: true, childList: true });
 }

681819c585a2cd9e4222bf8925ab959e89119d21
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2022-12-31 14:43:21 -0600

    Update find_or_observe_for_element.js

--- find_or_observe_for_element.js
+++ find_or_observe_for_element.js
@@ -1,42 +1,49 @@
 async function find_or_observe_for_element(query, func, parent_query, once) {
 
-	setTimeout(() => {
-		if (document.body === undefined) {
-			console.error("No body found");
-			return;
+    const body_promise = new Promise((resolve, reject) => {
+		let node = document.body;
+		if (node) {
+			resolve(node);
 		}
-	}, 6000);
+		const body_observer = new MutationObserver((mutations_list) => {
+            let node = document.body;
+            if (node) {
+                body_observer.disconnect();
+				resolve(node);
+            }
+        });
+        body_observer.observe(document.documentElement, {childList: true});
+		return;
+		const r = new Promise((resolve) => {
+			setTimeout(() => {
+				body_observer.disconnect();
+				resolve();
+			}, 3000);
+		});
+		r.then(reject(null));
+	});
 
-	while (document.body === undefined) {
-		setTimeout(() => {;}, 150);
+	console.debug("before: ",parent_query, query);
+
+	let parent_node = await body_promise.then(node => {return node;}, () => {return null});
+
+	console.debug("after: ", parent_node, parent_query, query);
+
+	if (parent_node === null) {
+		console.error("poopy");
+		return;
 	}
 
-	let parent_node;
-
-	if (parent_query === undefined) {
-		parent_node = document.body;
-	} else {
-        	setTimeout(() => {
-			if (document.body === undefined) {
-				console.warn("No custom parent found, falling back to body");
-				parent_node = document.body;
-			}
-		}, 10000);
-		while (parent_node === undefined) {
-		    parent_node = document.querySelector(parent_query);
-		    setTimeout(() => {;}, 150);
-        	}
-	}
 	const node_list = parent_node.querySelectorAll(query);
 	let match_list = [];
 	if (once && node_list.length) {
 		func(node_list[0]);
-	}else if (node_list.length) {	
+	}else if (node_list.length) {
 		for (const node of node_list) {
 			func(node);
 		}
 	}
-	const observer = new MutationObserver((mutations_list, observer) => {
+	const observer = new MutationObserver((mutations_list) => {
 		let match_list = [];
 		const compare_fn = (added_node) => {
 			if (added_node.matches(query)) {

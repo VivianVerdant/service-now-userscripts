4771633c4629b023196cc265027379a1e9a66695
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2023-09-20 19:48:32 -0500

    Update better_task.user.js

--- better_task.user.js
+++ better_task.user.js
@@ -3,45 +3,35 @@
 // @namespace    https://github.com/VivianVerdant/service-now-userscripts/tree/main
 // @homepageURL  https://github.com/VivianVerdant/service-now-userscripts/tree/main
 // @supportURL   https://github.com/VivianVerdant/service-now-userscripts/tree/main
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Vivian
 // @match        https://*.service-now.com/sc_task.do*
-// @require      https://github.com/VivianVerdant/service-now-userscripts/raw/main/find_or_observe_for_element.js
-// @grant        GM_getValue
+// @grant        GM_addStyle
 // ==/UserScript==
 
-/* globals  find_or_observe_for_element */
-
 (function() {
-    'use strict';
+'use strict';
+GM_addStyle(`
+[aria-label="Catalog Task form section"] > div:nth-child(2) > div > div.custom-form-group.is-wide.form-group.ng-scope {
+max-height: 158px;
+overflow-y: auto;
+overflow-x: clip;
+min-height: 58px;
+}
 
-    find_or_observe_for_element("#cxs_widget_container", (node) => {
-		console.log(node);
-        const widget = node.parentNode.parentNode.parentNode.parentNode.parentNode;
-        widget.style = "max-height: 158px; overflow-y: auto; overflow-x: clip; min-height: 58px;";
-	}, undefined, true);
+#output_messages,
+.horizontal-rule {
+display: none;
+}
 
-    find_or_observe_for_element("hr", (node) => {
-		console.log(node);
-        node.parentNode.style = "display: none;";
-	}, undefined, false);
+#cxs_widget_container tr:has(#cxs_maximize_results) {
+position: absolute;
+right: 0;
+}
 
-    find_or_observe_for_element("#cxs_maximize_results", (node) => {
-		console.log(node);
-        node.parentNode.parentNode.style = "position: absolute; right: 0;";
-        node.style = "z-index: 99999;";
-	}, undefined, true);
-
-    find_or_observe_for_element("#cxs_search_container", (node) => {
-		console.log(node);
-        //node.style = "padding: 0;";
-        node.firstElementChild.style = "padding-right: 180px;";
-	}, undefined, true);
-
-    find_or_observe_for_element("#output_messages", (node) => {
-		console.log(node);
-        node.style = "display: none;";
-	}, undefined, true);
-
+#cxs_search_container > div:first-child {
+padding-right: 180px;
+}
+`);
 })();

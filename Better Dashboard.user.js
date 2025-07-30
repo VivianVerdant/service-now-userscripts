4b26f7266d84bb0ef0b9c5777e2f6163f4bd37ba
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2025-07-30 08:32:22 -0500

    Update Better Dashboard.user.js

--- Better Dashboard.user.js
+++ Better Dashboard.user.js
@@ -1,27 +1,27 @@
 // ==UserScript==
 // @name         Better Dashboard
 // @namespace    https://github.com/VivianVerdant/service-now-userscripts
-// @version      0.1
+// @version      0.0.2
 // @description  Description
 // @author       Vivian
 // @match        https://*.service-now.com/*
 // @homepageURL  https://github.com/VivianVerdant/service-now-userscripts
 // @supportURL   https://github.com/VivianVerdant/service-now-userscripts/issues
-// @require      https://github.com/VivianVerdant/service-now-userscripts/raw/main/find_or_observe_for_element.js
+// @require      https://github.com/VivianVerdant/service-now-userscripts/raw/refs/heads/main/lib/wait_for_element.js
 // @resource     better_dashboard_css https://github.com/VivianVerdant/service-now-userscripts/raw/main/css/better_dashboard.css
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @run-at       document-start
 // ==/UserScript==
-/* globals find_or_observe_for_element*/
 
+/* globals wait_for_element*/
 
 /* Changelog
+    v0.0.2 - cleaned up code, switched to wait_for_element
     v0.1 - Initial release
 */
 
 var location = window.location.href;
-var run_once = false;
 var noc = 1;
 
 HTMLElement.prototype.addNode = function (type, id, classes) {
@@ -36,80 +36,27 @@ HTMLElement.prototype.addNode = function (type, id, classes) {
 	return new_node;
 };
 
-function overrideAJAX(){
-	var open = window.XMLHttpRequest.prototype.open,
-		load = window.XMLHttpRequest.prototype.load,
-		send = window.XMLHttpRequest.prototype.send;
-
-	function openReplacement(method, url, async, user, password) {
-		this._url = url;
-		//console.log("Open:", JSON.stringify(this), JSON.stringify(arguments));
-		return open.apply(this, arguments);
-	}
-
-	function sendReplacement(data) {
-		if(this.onreadystatechange) {
-			this._onreadystatechange = this.onreadystatechange;
-		}
-		this.onreadystatechange = onReadyStateChangeReplacement;
-
-        if (arguments["0"] && arguments["0"].includes("sysparm_synch=true")) {
-            //arguments["0"] = arguments["0"].replace("sysparm_synch=true", "sysparm_synch=false");
-            //console.warn("Send:", JSON.stringify(this), JSON.stringify(arguments));
-        }
-
-		return send.apply(this, arguments);
-
-	}
-
-	function onReadyStateChangeReplacement() {
-		if (this.readyState == 4 && this._url.includes("rectangle" )){
-			let r = JSON.parse(this.response);
-
-		}
-		if(this._onreadystatechange) {
-			return this._onreadystatechange.apply(this, arguments);
-		}
-	}
-
-	window.XMLHttpRequest.prototype.open = openReplacement;
-	window.XMLHttpRequest.prototype.send = sendReplacement;
-
-}
-
 async function main(element) {
     'use strict';
 
-	console.log("edit main");
-	if (run_once){
-		return
-	}
-	run_once = true;
-
     GM_addStyle(GM_getResourceText("better_dashboard_css"));
 
-    overrideAJAX();
-
-	find_or_observe_for_element("#resolve_incident", (node) => {
-	}, undefined, false);
-
-    find_or_observe_for_element(".list_group_toggle_overwrite", (node) => {
+    wait_for_element(".list_group_toggle_overwrite", (node) => {
         if (node.getAttribute("data-expanded") == "false") {
             node.click();
         }
-	}, undefined, false);
+	}, false);
 
-    find_or_observe_for_element("#header-container", (node) => {
+    wait_for_element("#header-container", (node) => {
         node.classList.add("small-header");
         node.onclick = (event) => {
             //event.preventDefualt();
             console.log("toggle");
             document.querySelector("#header-container").classList.toggle("small-header");
         }
-	}, undefined, false);
+	}, false);
 
-
-    find_or_observe_for_element("#clickHereIFrame", (node) => {
+    wait_for_element("#clickHereIFrame", (node) => {
         console.log(node);
         node = node.parentNode.parentNode.parentNode;
         const frame = document.createElement("iframe");
@@ -127,24 +74,22 @@ async function main(element) {
             noc += 1;
         } else {
             frame.setAttribute("src", "https://virteva.service-now.com/noc2.do");
-
         }
-	}, undefined, false);
 
+	}, false);
 
-/*    setTimeout(() => {
-        const header = document.querySelector("#noc-frame").contentDocument.head.addNode("style", "", []);
-        header.setAttribute("type", "text/css");
-        header.innerHTML = `#newevent_header,
-                                #newevent_footer {
-                                    font-size: 50px !important;
-                                }`;
-    }, 6000);*/
+    wait_for_element("a", (node) => {
+        node.setAttribute("target", "_blank");
+    }, false);
+
+    wait_for_element("button.icon-refresh", (node) => {
+        const intervalID = setInterval((node) => {node.click()}, 30000, node);
+    }, false);
+
 }
 
-console.warn("Better Dashboard Start");
 if (location.includes("pa_dashboard.do") || location.includes("noc1.do") || location.includes("noc2.do")){
-    console.warn(location);
-    main();
+    wait_for_element("body", (node) => {
+        main();
+    });
 }
-console.warn("Better Dashboard End");

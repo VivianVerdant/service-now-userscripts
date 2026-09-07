63c29a604fc36e332ec3a53eba7d18619bafa9ff
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2026-09-07 17:40:52 -0500

    Update servicenow_function_buttons.user.js

--- servicenow_function_buttons.user.js
+++ servicenow_function_buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Service-Now Function Buttons
 // @namespace    https://github.com/VivianVerdant/service-now-userscripts
-// @version      0.2.2
+// @version      0.3.0
 // @description  Add buttons to do stuff, I guess
 // @author       Vivian Roerig Willett
 // @homepageURL  https://github.com/VivianVerdant/service-now-userscripts
@@ -17,6 +17,87 @@
 /* globals page_header create_node create_button create_header_button add_header_button*/
 /* globals wait_for_element */
 /* globals GlideRecord g_form g_user g_navigation*/
+
+function email() {
+
+}
+
+function incident() {
+
+}
+
+function event() {
+    add_header_button("Informational",() => {
+        g_form.setValue("u_category", "Informational");
+        g_form.setValue("u_subcategory", "Escalation");
+        g_form.setValue("u_trouble_code", "96b743276fdc8a001ef4eef11c3ee4c9", "Informational");
+        setTimeout(() => {g_form.save();}, 200);
+    });
+
+    add_header_button("Switch Offline",() => {
+        g_form.setValue("u_category", "Network");
+        g_form.setValue("u_subcategory", "Switch");
+        g_form.setValue("u_trouble_code", "fd8128276fd31100ad775ddd5d3ee401", "Switch Offline");
+        setTimeout(() => {g_form.save();}, 200);
+    });
+
+    add_header_button("Firewall Offline",() => {
+        g_form.setValue("u_category", "Network");
+        g_form.setValue("u_subcategory", "Firewall");
+        g_form.setValue("u_trouble_code", "efb128276fd31100ad775ddd5d3ee4b2", "FIrewall Offline");
+        setTimeout(() => {g_form.save();}, 200);
+    });
+
+    add_header_button("VM Offline",() => {
+        g_form.setValue("u_category", "Infrastructure");
+        g_form.setValue("u_subcategory", "Vmware");
+        g_form.setValue("u_trouble_code", "c51164276fd31100ad775ddd5d3ee4b1", "Host Offline");
+        setTimeout(() => {g_form.save();}, 200);
+    });
+
+    add_header_button("Server Perf",() => {
+        g_form.setValue("u_category", "Infrastructure");
+        g_form.setValue("u_subcategory", "Server");
+        g_form.setValue("u_trouble_code", "a90011346f32f9006f41cf30be3ee46d", "Performance");
+        setTimeout(() => {g_form.save();}, 200);
+    });
+
+    add_header_button("UPS",() => {
+        g_form.setValue("u_category", "Facilities");
+        g_form.setValue("u_subcategory", "UPS");
+        g_form.setValue("u_trouble_code", "d257d1a06f146d40e2e1a14d5d3ee409", "UPS");
+        setTimeout(() => {g_form.save();}, 200);
+    });
+
+    add_header_button("VM CPU",() => {
+        g_form.setValue("u_category", "Infrastructure");
+        g_form.setValue("u_subcategory", "Vmware");
+        g_form.setValue("u_trouble_code", "3bf0a4276fd31100ad775ddd5d3ee403", "CPU");
+        setTimeout(() => {g_form.save();}, 200);
+    });
+
+    add_header_button("Auto Cleared",() => {
+        g_form.setValue("state", 3);
+        g_form.setValue("u_close_code", "Auto-Cleared");
+        setTimeout(() => {g_form.save();}, 200);
+    }, "icon-checkbox-checked");
+}
+
+function kb() {
+
+}
+
+var handled_user_ids = [];
+
+/* INDEV
+wait_for_element(".sn-card-component:has([data-presence-id])", (node) => {
+    const id = node.querySelector("[data-presence-id]").getAttribute("data-presence-id");
+    if (!handled_user_ids.includes(id)) {
+        console.warn(id);
+    }
+}, false);
+*/
+
 (function() {
     'use strict';
 
@@ -51,84 +132,11 @@
         case "/kb":
             kb();
             break
-        case "/sc_task.do":
-            task();
+        case "/kb_view.do":
+            kb();
             break
         default:
             break
     }
 
 })();
-
-async function fix_text_area(e) {
-    e.style.height = "0px";
-    e.style.height = e.scrollHeight + 8 + "px";
-    setTimeout(() => {e.click();}, 100);
-}
-
-const display_name_regex = /(?<=DisplayName\s*:\s*)(.*)/g;
-const email_regex = /(?<=UserPrincipalName\s*:\s*)(.*)/g;
-
-function task() {
-    const page_type = "task";
-    wait_for_element("[id='element.sc_task.request_item.company']", (node) => {
-        const company_sys_id = document.getElementById("sc_task.request_item.company").getAttribute("value");
-        switch (company_sys_id){
-            case "59563f611b5d94d09a47524d0d4bcb45":
-                archwell_task();
-                break
-        }
-    });
-}
-
-function email() {
-    const page_type = "email";
-    add_header_button("Email",()=>{g_form.addInfoMessage(page_type);});
-}
-
-function incident() {
-    const page_type = "email";
-    add_header_button("Incident",()=>{g_form.addInfoMessage(page_type);});
-}
-
-function event() {
-    const page_type = "email";
-    add_header_button("Event",()=>{g_form.addInfoMessage(page_type);});
-}
-
-function kb() {
-    const page_type = "email";
-    add_header_button("KB",()=>{g_form.addInfoMessage(page_type);});
-}
-
-wait_for_element("textarea", (node) => {
-    //console.log('textarea has been added:-------------------------------------------');
-    //console.debug(node);
-    const text_area_fn = async (e) => {e.target.style.height = "0px"; e.target.style.height = e.target.scrollHeight + 8 + "px";}
-    node.addEventListener("change", text_area_fn);
-    node.addEventListener("keydown", text_area_fn);
-    node.addEventListener("click", text_area_fn);
-    setTimeout(() => {node.click();}, 1000);
-}, false);
-
-function archwell_task() {
-    add_header_button("AD Comment",()=>{
-        try {
-            const work_notes = document.querySelector(".h-card-wrapper.activities-form").innerText;
-            const display_name = display_name_regex.exec(work_notes)[0];
-            const email = email_regex.exec(work_notes)[0];
-            document.getElementById("tabs2_section").querySelector(".tab_header:nth-of-type(2) > span").click();
-            const comments = document.getElementById("sc_task.request_item.comments");
-            const requested_for = document.querySelector(".form-group.sc-row:has([aria-label='Requested for']) .input-group input").value;
-            comments.value = `Hello ${requested_for},\n`
-                + `This account has been created per your request\n`
-                + `Display Name: ${display_name}\n`
-                + `Email: ${email}\n`
-                + `\n`
-                + `Thank you,\n`
-                + `${g_user.firstName}\n`
-                + `Request Fulfillment`;
-            fix_text_area(comments);
-        } catch(e) {}
-    });
-}
